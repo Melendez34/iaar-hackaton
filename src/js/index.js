d28a51ce8df731c3ebf348ea1aeb078f52cc6df5
Omar Melendez <omar.mel.lopez@gmail.com>
2022-04-16 17:17:48 -0500

    3. agregando modelo ejemplo

--- src/js/index.js
+++ src/js/index.js
@@ -4,4 +4,5 @@ const showcase = document.querySelector(".showcase");
 menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
   showcase.classList.toggle("active");
+  menuToggle.classList.add("blue");
 });
