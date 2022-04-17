bc7beb36d352a6332544004dc976ceffa8b4358d
Omar Melendez <omar.mel.lopez@gmail.com>
2022-04-17 00:59:34 -0500

    5. Agregando modelos 3d y sitio Model.html

--- src/js/index.js
+++ src/js/index.js
@@ -4,5 +4,4 @@ const showcase = document.querySelector(".showcase");
 menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
   showcase.classList.toggle("active");
-  menuToggle.classList.add("blue");
 });
